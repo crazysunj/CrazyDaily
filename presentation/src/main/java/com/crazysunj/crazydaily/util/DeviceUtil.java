c7cbb1ddd0b9dc292412eff0db1356ab44358d26
crazysunj <twsunj@gmail.com>
2018-11-12 13:46:41 +0800

    添加全面屏判断

--- presentation/src/main/java/com/crazysunj/crazydaily/util/DeviceUtil.java
+++ presentation/src/main/java/com/crazysunj/crazydaily/util/DeviceUtil.java
@@ -15,10 +15,23 @@
  */
 package com.crazysunj.crazydaily.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.DisplayCutout;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowManager;
 
 import com.crazysunj.crazydaily.BuildConfig;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
 
 /**
  * @author: sunjian
@@ -42,5 +55,51 @@ public class DeviceUtil {
     public static int getStatusBarHeight(Context context) {
         final Resources resources = context.getResources();
         return resources.getDimensionPixelSize(resources.getIdentifier("status_bar_height", "dimen", "android"));
+    }
+
+    /**
+     * 判断全面屏
+     *
+     * @param activity Activity
+     * @return boolean
+     */
+    public static boolean isFullScreenDisplay(@NonNull Activity activity) {
+        if (isSupportP()) {
+            // 谷歌官方提供
+            Window window = activity.getWindow();
+            if (window != null) {
+                WindowInsets windowInsets = window.getDecorView().getRootWindowInsets();
+                if (windowInsets != null) {
+                    DisplayCutout displayCutout = windowInsets.getDisplayCutout();
+                    if (displayCutout != null) {
+                        List<Rect> rects = displayCutout.getBoundingRects();
+                        return rects != null && rects.size() > 0;
+                    }
+                }
+            }
+        }
+
+        WindowManager wm = (WindowManager) activity.getSystemService(Context.WINDOW_SERVICE);
+        if (wm != null) {
+            Display defaultDisplay = wm.getDefaultDisplay();
+            if (defaultDisplay != null) {
+                DisplayMetrics dm = new DisplayMetrics();
+                defaultDisplay.getRealMetrics(dm);
+                float heightPixels = dm.heightPixels;
+                float widthPixels = dm.widthPixels;
+                // 通过分比率比例去判断
+                return heightPixels / widthPixels >= 1.96f;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 是否支持Android9.0
+     *
+     * @return boolean
+     */
+    public static boolean isSupportP() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.P;
     }
 }
